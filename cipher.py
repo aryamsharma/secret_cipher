2d4993810fce1cd65881ab7dd87ccf82545ec0fc
aryamsharma <32722100+aryamsharma@users.noreply.github.com>
2020-06-23 04:29:22 -0400

    added a logger as well just because i have issues

--- cipher.py
+++ cipher.py
@@ -34,6 +34,30 @@ class CF:
         print(f"org      : {text}")
         print(f"encrypted: {cipher.encrypt(text)}")
         print(f"decrypted: {cipher.decrypt(cipher.encrypted)}")
+        cipher.log(text, "di")
+
+    def log(self, text, mode):
+        to_write = [
+            "#################################################\n",
+            f"Mode     : {mode}\n",
+            f"seed     : {self.seed}\n",
+            f"chars    : {''.join(self.chars)}\n",
+            f"key      : {''.join(self.key)}\n",
+            f"org      : {text}\n",
+            f"encrypted: {cipher.encrypt(text)}\n",
+        ]
+        if mode == "e":
+            to_write.append(f"decrypted: {cipher.decrypt(cipher.encrypted)}\n")
+
+        elif mode == "d":
+            to_write.append(f"decrypted: {cipher.decrypt(text)}\n")
+
+        elif mode == "di":
+            to_write.append(f"decrypted: {cipher.decrypt(cipher.encrypted)}\n")
+            to_write.append(f"decrypted: {cipher.decrypt(text)}\n")
+
+        with open("Log.txt", "a") as f:
+            f.writelines(to_write)
 
 
 if __name__ == "__main__":
@@ -41,17 +65,20 @@ if __name__ == "__main__":
 
     cipher = CF()
 
-    if to_do == "di":
-        cipher.dignostic()
-        exit()
-
-    text = input("Enter str: ").strip()
-
     if to_do == "e":
+        text = input("Enter str: ").strip()
         print(f"encrypted: {cipher.encrypt(text)}")
 
     elif to_do == "d":
+        text = input("Enter str: ").strip()
         print(f"decrypted: {cipher.decrypt(text)}")
+
+    elif to_do == "di":
+        cipher.dignostic()
+        exit()
 
     else:
         print("Wrong input please execute again")
+        exit()
+
+    cipher.log(text, to_do)
