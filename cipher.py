0019ef02ad4553850ce4c46e86e94dc8ea2b0366
aryamsharma <32722100+aryamsharma@users.noreply.github.com>
2020-06-23 03:36:45 -0400

    used OOB style and cleaned it up so its not so bad to look at

--- cipher.py
+++ cipher.py
@@ -1,43 +1,38 @@
 import time
-import math
 import random
 
-def random_seed_value():
-    return time.strftime("%Y%m%d")
+class CF:
+    def __init__(self):
+        self.chars = [i for i in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&\'()\\*+,-./:;<=>?@[]^_`{|}~ "]
+        self.key = self.chars[:]
+        self.seed = time.strftime("%Y%m%d")
+        random.seed(self.seed)
+        random.shuffle(self.key)
 
-def key_randomize(chars):
-    key = chars[:]
-    random.shuffle(key)
-    return key
+    def encrypt(self, text):
+        self.encrypted = ""
+        for k, v in enumerate(text):
+            pos = self.chars.index(v)
+            self.encrypted += self.key[pos]
+        return self.encrypted
 
-def encrypting(text, key, chars):
-    new_text = ""
-    for k, v in enumerate(text):
-        pos = chars.index(v)
-        new_text += key[pos]
-    return new_text
-
-
-def decrypting(text, key, chars):
-    new_text = ""
-    for k, v in enumerate(text):
-        pos = key.index(v)
-        new_text += chars[pos]
-    return new_text
+    def decrypt(self, text):
+        self.decrypted = ""
+        for k, v in enumerate(text):
+            pos = self.key.index(v)
+            self.decrypted += self.chars[pos]
+        return self.decrypted
 
 
 if __name__ == "__main__":
-    seed = random_seed_value()
-    random.seed(seed)
-    chars = [i for i in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&\'()\\*+,-./:;<=>?@[]^_`{|}~ "]
-    key = key_randomize(chars)
     text = input("Enter str: ").strip()
-    encrypted_text = encrypting(text, key, chars)
-    decrypted_text = decrypting(encrypted_text, key, chars)
+    # text = "test"
+    cipher = CF()
+    print(cipher.encrypt(text))
     
-    print(f"seed     : {seed}")
-    print(f"chars    : {''.join(chars)}")
-    print(f"key      : {''.join(key)}")
+    print(f"seed     : {cipher.seed}")
+    print(f"chars    : {''.join(cipher.chars)}")
+    print(f"key      : {''.join(cipher.key)}")
     print(f"org      : {text}")
-    print(f"encrypted: {encrypted_text}")
-    print(f"decrypted: {decrypted_text}")
+    print(f"encrypted: {cipher.encrypt(text)}")
+    print(f"decrypted: {cipher.decrypt(cipher.encrypted)}")
